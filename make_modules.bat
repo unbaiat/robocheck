96c698c0dc667342975b9b2ee5f7efb7dcf1045c
Laura Vasilescu <laura@rosedu.org>
2012-08-18 19:30:07 +0300

    modules

--- make_modules.bat
+++ make_modules.bat
@@ -18,10 +18,10 @@
 				cd ../..
 				@exit /B 1
 			)
-			cp /*.dll ../.. 2>NUL
-			cp /*.lib ../.. 2>NUL
-			cp /*.exp ../.. 2>NUL
-			cp /*.jar ../.. 2>NUL
+			cp *.dll ..\.. 2>NUL
+			cp *.lib ..\.. 2>NUL
+			cp *.exp ..\.. 2>NUL
+			cp *.jar ..\.. 2>NUL
 		)
 		@cd ../..
 	)
